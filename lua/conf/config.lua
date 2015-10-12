c4d9f0d0ba9cc50e0b563c57adece04c02cc5c71
Waldemar Hummer <hummer@infosys.tuwien.ac.at>
2015-10-12 16:48:49 +0200

    add dev. config to config.lua

--- lua/conf/config.lua
+++ lua/conf/config.lua
@@ -1,11 +1,11 @@
 local M = { }
-M.redis_host = '127.0.0.1'
+M.redis_host = 'redis.development.svc.cluster.local'
 M.redis_port = '6379'
 M.redis_password = ''
 M.redis_timeout = 5000
 M.redis_keepalive_pool_size = 100
 M.redis_keepalive_max_idle_timeout = 30000
-M.max_path_length = 1
+M.max_path_length = 3
 M.session_length = 0
 M.default_score = 0
 M.plugins = {'random'}
